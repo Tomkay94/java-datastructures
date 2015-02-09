573f3e5aaceaae37258dab00e9ed46fda9215abd
karysto <thanasi.karachotzitis@mail.utoronto.ca>
2015-02-09 02:27:35 -0500

    Basic linked list class.

---
+++ LinkedList/Node.java
@@ -0,0 +1,17 @@
+public class Node {
+
+  Node next = null;
+  int data;
+
+  public Node(int d) {
+    data = d;
+  }
+
+  // Example
+  public static void main(String[] args) {
+
+    Node head = new Node(10);
+
+  }
+
+}
