c737b608ddbd9798bb1e15bcdc604bed6d7c08b4
karysto <thanasi.karachotzitis@mail.utoronto.ca>
2015-02-10 23:52:21 -0500

    Implement linked list findMedianElement.

--- LinkedList/Node.java
+++ LinkedList/Node.java
@@ -29,15 +29,37 @@ public class Node {
   }
 
   // Find the median element in the linked list
-  private void findMedianElement() {
+  private Node findMedianElement() {
+    Node curr     = this;
+    Node hopOnce  = curr;
+    Node hopTwice = curr;
+
+    while(true) {
+
+      // Weve reached the end of the list, hopOnce is the median.
+      if (hopTwice.next == null) {
+        System.out.println("End Node Value: \t"    + hopTwice.data);
+        System.out.println("Median Node Value: \t" + hopOnce.data);
+        return hopOnce;
+      }
+
+      // hopTwice is one Node from the end, go to the end
+      else if (hopTwice.next.next == null) {
+        hopTwice = hopTwice.next;
+      }
+
+      else {
+        hopOnce = hopOnce.next;
+        hopTwice = hopTwice.next.next;
+      }
+    }
   }
 
 
-  // Example
+  // Example function calls
   public static void main(String[] args) {
 
     Node head = new Node(10);
-
   }
 
 }
