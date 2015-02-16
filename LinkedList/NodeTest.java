45f40fe9682dc407c8923f4ba18a0e7bbb7d7b2a
karysto <thanasi.karachotzitis@mail.utoronto.ca>
2015-02-16 00:35:50 -0500

    Refactor some test cases.

--- LinkedList/NodeTest.java
+++ LinkedList/NodeTest.java
@@ -63,20 +63,33 @@ public class NodeTest {
 
   @Test
   public void testOneNodeFindMedian() {
-    Node head      = new Node(10);
-    Node afterHead = new Node(15);
+    Node head       = new Node(10);
 
-    head.appendToTail(afterHead);
-    Assert.assertEquals(head.next, afterHead);
+    Assert.assertEquals(head.findMedianElement(), head.data);
   }
 
   @Test
   public void testOddNodeFindMedian() {
-    Node head      = new Node(10);
-    Node afterHead = new Node(15);
+    Node head       = new Node(5);
+    head.appendToTail(new Node(10));
+    head.appendToTail(new Node(15));
 
-    head.appendToTail(afterHead);
-    Assert.assertEquals(head.next, afterHead);
+    Node medianNode = head.findMedianElement();
+
+    Assert.assertEquals(medianNode.data, 10);
+  }
+
+
+  @Test
+  public void testEvenNodeFindMedian() {
+    Node head       = new Node(10);
+    head.appendToTail(new Node(15));
+    head.appendToTail(new Node(20));
+    head.appendToTail(new Node(25));
+
+    Node medianNode = head.findMedianElement();
+
+    Assert.assertEquals(medianNode, 15);
   }
 
   @Test
