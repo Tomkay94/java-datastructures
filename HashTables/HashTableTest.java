eee4cf6fe7df388f9a40222a696b2e9444e79264
karysto <thanasi.karachotzitis@mail.utoronto.ca>
2015-02-16 18:36:24 -0500

    Add setup and teardown to hash table test suite.

--- HashTables/HashTableTest.java
+++ HashTables/HashTableTest.java
@@ -2,30 +2,45 @@ import org.junit.*;
 
 public class HashTableTest {
 
+  private HashTable ht;
+
+  @Before
+  public void init() {
+    ht = new HashTable(17);
+  }
+
+  @After
+  public void clear() {
+    ht = null;
+  }
+
   @Test
   public void testInsertEntry() {
-    HashTable ht = new HashTable(17);
     ht.hashFunctionMod("5");
+
+    // Assert here
   }
 
   @Test
   public void testFindKeyLast() {
-    HashTable ht = new HashTable(17);
     ht.hashFunctionMod("35");
     ht.hashFunctionMod("40");
     ht.hashFunctionMod("45");
     ht.hashFunctionMod("50");
     ht.hashFunctionMod("55");
+
+    // Assert here
   }
 
   @Test
   public void testFindKeyFirst() {
-    HashTable ht = new HashTable(17);
     ht.hashFunctionMod("35");
     ht.hashFunctionMod("40");
     ht.hashFunctionMod("45");
     ht.hashFunctionMod("50");
     ht.hashFunctionMod("55");
+
+    // Assert here
   }
 
 }
