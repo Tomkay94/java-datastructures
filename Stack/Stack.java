c7b60a8aa6199e21cf966ebdcd000f85eac1026e
karysto <thanasi.karachotzitis@mail.utoronto.ca>
2015-02-17 01:46:40 -0500

    Implement Stack. Write test case skeleton. Add makefile.

---
+++ Stack/Stack.java
@@ -0,0 +1,46 @@
+
+/* Implements the Stack data structure. */
+
+public class Stack {
+
+  private int size;
+  private int capacity;
+  private String[] stackArray;
+
+  public Stack(int capacity) {
+    this.capacity = capacity;
+    this.stackArray = new String[capacity];
+  }
+
+  /* Push the new item to the top of the stack. */
+  protected void push(String item) {
+    this.stackArray[size] = item;
+    this.size++;
+  }
+
+  /* Pop the item off the top of the stack. */
+  protected void pop() {
+    this.stackArray[size - 1] = null;
+    this.size--;
+  }
+
+  /* Determine if the stack is empty. */
+  protected boolean isEmpty() {
+    return this.size == 0;
+  }
+
+  /* Determine if the stack still has space for more elements. */
+  protected boolean hasSpace() {
+    return this.size < this.capacity;
+  }
+
+  /* Show the elements in the stack */
+  protected void displayStack() {
+    for (String item : this.stackArray) {
+      if (item != null) {
+      System.out.println(item);
+      }
+    }
+  }
+
+}
---
+++ Stack/StackTest.java
@@ -0,0 +1,67 @@
+import org.junit.*;
+
+public class StackTest {
+
+  Stack stk;
+
+  @Before
+  public void setUp() {
+    stk = new Stack(5);
+  }
+
+  @After
+  public void tearDown() {
+    stk = null;
+  }
+
+  @Test
+  public void testPushOnEmpty() {
+
+  }
+
+  @Test
+  public void testPushOnFull() {
+
+  }
+
+  @Test
+  public void testPushOnStack() {
+
+  }
+
+  @Test
+  public void testPopOnEmpty() {
+
+  }
+
+  @Test
+  public void testPopOnFull() {
+
+  }
+
+  @Test
+  public void testPopOnRegular() {
+
+  }
+
+  @Test
+  public void testIsEmptyOnFull() {
+
+  }
+
+  @Test
+  public void testIsEmptyOnEmpty() {
+
+  }
+
+  @Test
+  public void testHasCapacityOnEmpty() {
+
+  }
+
+  @Test
+  public void testHasCapacityOnFull() {
+
+  }
+
+}
