3435db02b6bbc4835e0a486c0eee9a21e7ead8f6
karysto <thanasi.karachotzitis@mail.utoronto.ca>
2015-02-17 02:20:17 -0500

    Add Queue skeleton methods.

---
+++ Queue/Queue.java
@@ -0,0 +1,31 @@
+
+/* Implements the Queue data structure. */
+
+public class Queue {
+
+  private size;
+  private capacity;
+  private String[] queueArray;
+
+  public Queue(int capacity) {
+    this.capacity = capacity;
+    this.queueArray = new String[capacity];
+  }
+
+  protected void enqueue(String item) {
+
+  }
+
+  protected String dequeue() {
+
+  }
+
+  protected void hasCapacity() {
+
+  }
+
+  protected void isEmpty() {
+
+  }
+
+}
