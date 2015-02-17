53c9035197af469062bd497c92421a46f0fa5ffe
karysto <thanasi.karachotzitis@mail.utoronto.ca>
2015-02-16 20:43:05 -0500

    Add skeleton test cases for hash map.

---
+++ HashMap/HashMapTest.java
@@ -0,0 +1,32 @@
+import org.junit.*;
+
+public class HashMapTest {
+
+  HashMap hm;
+
+  @Before
+  public void init() {
+    hm = new HashMap(7);
+  }
+
+  @Test
+  public void testInsertSingleKey() {
+
+  }
+
+  @Test
+  public void testInsertDuplicateKey() {
+
+  }
+
+  @Test
+  public void testFindExistingKey() {
+
+  }
+
+  @Test
+  public void testInsertInvalidKey() {
+
+  }
+
+}
