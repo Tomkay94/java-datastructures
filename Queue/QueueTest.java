7c52b1d8c1b4a0775a7e334bf05db8e9c64dd055
karysto <thanasi.karachotzitis@mail.utoronto.ca>
2015-02-17 03:26:11 -0500

    Add Queue skeleton test cases.

---
+++ Queue/QueueTest.java
@@ -0,0 +1,56 @@
+import org.junit.*;
+
+public class QueueTest {
+
+  Queue q;
+
+  @Before
+  public void setUp() {
+    q = new Queue(5);
+  }
+
+  @After
+  public void tearDown() {
+    q = null;
+  }
+
+  @Test
+  public void testIsEmptyOnEmpty() {
+
+  }
+
+  @Test
+  public void testIsEmpty() {
+
+  }
+
+  @Test
+  public void testHasCapacityOnEmpty() {
+
+  }
+
+  @Test
+  public void testHasCapacityOnFull() {
+
+  }
+
+  @Test
+  public void testEnqueueOnEmpty() {
+
+  }
+
+  @Test
+  public void testEnqueueOnFull() {
+
+  }
+
+  @Test
+  public void testDequeueOnEmpty() {
+
+  }
+
+  @Test
+  public void testDequeueOnFull() {
+
+  }
+}
