ad2b61abbda0a2b5fea5d81eb215a451d8e47509
karysto <thanasi.karachotzitis@mail.utoronto.ca>
2015-02-17 20:03:15 -0500

    Implement BST findNode, post, pre and in order traversals.

--- BinarySearchTree/BinarySearchTree.java
+++ BinarySearchTree/BinarySearchTree.java
@@ -11,7 +11,7 @@ public class BinarySearchTree {
     Node newNode = new Node(key, data);
 
     /* First node in the tree */
-    if (root = null) {
+    if (root == null) {
       root = newNode;
       return;
     }
@@ -39,7 +39,7 @@ public class BinarySearchTree {
           focusNode = focusNode.rightChild;
 
           if (focusNode == null) {
-            parent.rightChild = newNode
+            parent.rightChild = newNode;
             return;
           }
         }
@@ -47,11 +47,50 @@ public class BinarySearchTree {
     }
   }
 
+  protected Node findNode(int key) {
+    Node focusNode = root;
+
+    while (focusNode.key != key) {
+      if (key < focusNode.key) {
+          /* Shift the focusNode left */
+          focusNode = focusNode.leftChild;
+      }
+
+      else if (key > focusNode.key){
+        /* Shift the focusNode right */
+        focusNode = focusNode.rightChild;
+      }
+
+      if (focusNode == null) {
+        return null;
+      }
+    }
+    return focusNode;
+  }
+
   protected void traverseInOrder(Node focusNode) {
     if (focusNode != null) {
       traverseInOrder(focusNode.leftChild);
       System.out.println(focusNode);
       traverseInOrder(focusNode.rightChild);
+    }
+    return;
+  }
+
+  protected void traversePreOrder(Node focusNode) {
+    if (focusNode != null) {
+      System.out.println(focusNode);
+      traversePreOrder(focusNode.leftChild);
+      traversePreOrder(focusNode.rightChild);
+    }
+    return;
+  }
+
+  protected void traversePostOrder(Node focusNode) {
+    if (focusNode != null) {
+      traversePostOrder(focusNode.leftChild);
+      traversePostOrder(focusNode.rightChild);
+      System.out.println(focusNode);
     }
     return;
   }
