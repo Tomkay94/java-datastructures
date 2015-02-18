59f723f58e2d2d015f9ea1323f54c8ccf3a60d1f
karysto <thanasi.karachotzitis@mail.utoronto.ca>
2015-02-17 19:35:37 -0500

    Refactor BST node and BST.

--- BinarySearchTree/BinarySearchTree.java
+++ BinarySearchTree/BinarySearchTree.java
@@ -1,25 +1,59 @@
 
 public class BinarySearchTree {
 
-  private BSTNode root;
+  private Node root;
 
   public BinarySearchTree() {
     root = null;
   }
 
-  protected void insert(int value) {
-    if (root == null) {
-      root = new BSTNode(value);
+  protected void insert(int key, String data) {
+    Node newNode = new Node(key, data);
+
+    /* First node in the tree */
+    if (root = null) {
+      root = newNode;
       return;
     }
 
+    /* Figure out where to put the newNode*/
     else {
-      return root.add(value);
+      Node focusNode = root;
+      Node parent;
+
+      while(true) {
+        parent = focusNode;
+
+        /* newNode goes on the left subtree */
+        if (key < focusNode.key) {
+          focusNode = focusNode.leftChild;
+
+          if (focusNode == null) {
+            parent.leftChild = newNode;
+            return;
+          }
+        }
+
+        /* newNode goes on the right subtree */
+        else if (key > focusNode.key) {
+          focusNode = focusNode.rightChild;
+
+          if (focusNode == null) {
+            parent.rightChild = newNode
+            return;
+          }
+        }
+      }
     }
   }
 
-  protected BSTNode delete(int value) {
-
+  protected void traverseInOrder(Node focusNode) {
+    if (focusNode != null) {
+      traverseInOrder(focusNode.leftChild);
+      System.out.println(focusNode);
+      traverseInOrder(focusNode.rightChild);
+    }
+    return;
   }
 
 }
