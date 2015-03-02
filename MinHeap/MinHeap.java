a43bae0051736e9f09fc73e6c4b1f8aa0b13f36c
karysto <thanasi.karachotzitis@mail.utoronto.ca>
2015-03-02 04:10:33 -0500

    Add MinHeap test cases for insert and element retrieve methods.

--- MinHeap/MinHeap.java
+++ MinHeap/MinHeap.java
@@ -3,68 +3,90 @@
 
 public class MinHeap {
 
-  private int[] heapArray;
   private int size;
+  private int[] heapArray;
 
   public MinHeap(int size) {
     this.size = 0;
     this.heapArray = new int[size];
   }
 
+  /* Return the root of the heap without removing it. */
   protected int getMin() {
     if (!this.isEmpty()) {
-      return heapArray[0];
+      return this.heapArray[0];
     }
     return -1;
   }
 
+  /* Return true if the given index is a leaf in the heap. */
   protected boolean isLeaf(int index) {
     return (index > (this.size / 2)) && (index < this.size);
   }
 
+  /* Return true if the heap has no elements. */
   protected boolean isEmpty() {
     return this.size == 0;
   }
 
+  /* Return the left child index of index. */
   protected int getLeftChildIndex(int index) {
     return 2 * index + 1;
   }
 
+  /* Return the right child index of index. */
   protected int getRightChildIndex(int index) {
     return 2 * index + 2;
   }
 
+  /* Return the parent index of index.
+     Return -1 if the index is the root. */
   protected int getParentIndex(int index)  {
-    return (index - 1) / 2;
+    if (index > 0) {
+      return (index - 1) / 2;
+    }
+    return -1;
   }
 
+  /* Insert an element with value into the heap. */
   protected boolean insert(int value) {
-    if (!this.isEmpty()) {
-      size++;
-      heapArray[size - 1] = value;
-      siftUp(size - 1);
+    if (this.size < this.heapArray.length) {
+      ++this.size;
+      this.heapArray[size - 1] = value;
+      this.siftUp(size - 1);
       return true;
     }
     return false;
   }
 
+  /* Traverse up the heap and swap elements which do not fulfill the
+     min-heap property. */
   protected void siftUp(int index) {
-    int parentIndex, tmp;
+    int parentIndex;
+    int tmp;
+
     if (index != 0) {
-      parentIndex = getParentIndex(index);
-      if (heapArray[parentIndex] > heapArray[index]) {
-        tmp = heapArray[parentIndex];
-        heapArray[parentIndex] = heapArray[index];
-        heapArray[index] = tmp;
-        siftUp(parentIndex);
+      parentIndex = this.getParentIndex(index);
+      if (this.heapArray[parentIndex] > this.heapArray[index]) {
+        tmp = this.heapArray[parentIndex];
+        this.heapArray[parentIndex] = this.heapArray[index];
+        this.heapArray[index] = tmp;
+        this.siftUp(parentIndex);
       }
     }
   }
 
+  /* Return the element at index. */
+  protected int getElementByIndex(int index) {
+    return this.heapArray[index];
+  }
+
+  /* Return the array representing the heap. */
   protected int[] getHeapArray() {
     return this.heapArray;
   }
 
+  /* Return the number of elements in the heap. */
   protected int getSize() {
     return this.size;
   }
--- MinHeap/MinHeapTest.java
+++ MinHeap/MinHeapTest.java
@@ -29,14 +29,74 @@ public class MinHeapTest {
 
   @Test
   public void testIsEmptyOnNonEmpty() {
-    heap.insert(50);
+    heap.insert(5);
 
-    assertTrue(heap.isEmpty());
+    assertFalse(heap.isEmpty());
   }
 
   @Test
   public void testGetMinEmptyHeap() {
 
     assertEquals(heap.getMin(), -1);
+  }
+
+  @Test
+  public void testGetMinInsertedInOrder() {
+    heap.insert(5);
+    heap.insert(50);
+    heap.insert(500);
+    heap.insert(5000);
+
+    assertEquals(heap.getMin(), 5);
+  }
+
+  @Test
+  public void testGetMinInsertedUnsortedOrder() {
+    heap.insert(500);
+    heap.insert(50);
+    heap.insert(5);
+    heap.insert(5000);
+
+    assertEquals(heap.getMin(), 5);
+  }
+
+  @Test
+  public void testInsertUpdatesSize() {
+    heap.insert(5);
+    heap.insert(50);
+    heap.insert(500);
+    heap.insert(5000);
+
+    assertEquals(heap.getSize(), 4);
+  }
+
+  @Test
+  public void testGetRootLeftChildOnFullHeap() {
+    heap.insert(5);
+    heap.insert(50);
+    heap.insert(500);
+    heap.insert(5000);
+
+    assertEquals(heap.getElementByIndex(heap.getLeftChildIndex(0)), 50);
+  }
+
+  @Test
+  public void testGetRootRightChildOnFullHeap() {
+    heap.insert(5);
+    heap.insert(50);
+    heap.insert(500);
+    heap.insert(5000);
+
+    assertEquals(heap.getElementByIndex(heap.getRightChildIndex(0)), 500);
+  }
+
+  @Test
+  public void testGetParentOnFullHeap() {
+    heap.insert(5);
+    heap.insert(50);
+    heap.insert(500);
+    heap.insert(5000);
+
+    assertEquals(heap.getElementByIndex(heap.getParentIndex(2)), 5);
   }
 }
