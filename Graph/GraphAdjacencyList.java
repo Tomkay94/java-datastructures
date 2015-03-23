a0dd908dcddbf3899a2acd7361a063ceac48df48
karysto <thanasi.karachotzitis@mail.utoronto.ca>
2015-03-22 20:58:10 -0400

    Add stubs for GraphAdjacencyList methods.

--- Graph/GraphAdjacencyList.java
+++ Graph/GraphAdjacencyList.java
@@ -1,12 +1,39 @@
 import java.util.List;
 import java.util.ArrayList;
 
-/* Implements an adjacency list. */
+/* Implements an adjacency list using linked lists. */
+
 class GraphAdjacencyList<T> {
 
-  private ArrayList<Node<T>> adjacencyList;
+  private List<Node<T>> adjacencyList;
 
-  public GraphAdjacencyList() {
-    this.adjacencyList = new ArrayList<Node<T>>();
+  public GraphAdjacencyList(int size) {
+    this.adjacencyList = new ArrayList<Node<T>>(size);
+  }
+
+  /* Return true if firstNode points to secondNode. */
+  protected boolean hasDirectedEdge(T firstNode, T secondNode) {
+    return true;
+  }
+
+  /* Return true if both firstNode and secondNode point to each other. */
+  protected boolean hasUndirectedEdge(T firstNode, T secondNode) {
+    return true;
+  }
+
+  /* Add an edge from firstNode to secondNode. */
+  protected void addDirectedEdge(T firstNode, T secondNode) {
+    return;
+  }
+
+  /* Finds the linked list starting with node value findData. */
+  private Node getNode(T findData) {
+    Node current = this;
+
+    while(current.data != findData) {
+      current = current.next;
+    }
+
+    return current;
   }
 }
