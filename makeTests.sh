740398952fe612e9b6a2a4d5cfa110779ab73c3e
karysto <thanasi.karachotzitis@mail.utoronto.ca>
2015-03-25 19:32:26 -0400

    Remove unused vars from global test script.

--- makeTests.sh
+++ makeTests.sh
@@ -4,10 +4,6 @@
 # Run all test suites #
 # ################### #
 
-JVM=java
-JVC=javac
-TEST=org.junit.runner.JUnitCore
-
 for dir in $(ls -d */); do
   dirFile=${dir%%/*} # Remove end slash from dir
   echo '==============================='
