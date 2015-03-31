8ffc41122cfd2d34b77c43e871877a68f829df69
karysto <thanasi.karachotzitis@mail.utoronto.ca>
2015-03-31 05:18:35 -0400

    Remove ArrayList. Use HeadNodes.

--- AdjacencyList/AdjacencyList.java
+++ AdjacencyList/AdjacencyList.java
@@ -1,71 +1,87 @@
-import java.util.List;
-import java.util.ArrayList;
 
 /* Implements an adjacency list using linked lists. */
 
-class AdjacencyList<T> {
+/*
+Type structure of the adjacency list:
 
-  private List<Node<T>> adjacencyList;
+  HeadNode -> Node -> Node -> Node -> ... -> null
+     V
+  HeadNode -> Node -> ... -> null
+     V
+  HeadNode -> Node -> Node -> ... -> null
+     V
+   null
+*/
+public class AdjacencyList<T> {
+
   private int numEdges;
+  private HeadNode<T> adjList;
 
   public AdjacencyList() {
     this.numEdges = 0;
-    this.adjacencyList = new ArrayList<Node<T>>();
+    this.adjList  = null;
   }
 
-  /* Return true if firstNode points to secondNode. */
-  protected boolean hasDirectedEdge(T firstNode, T secondNode) {
+  /* Return true if firstData points to secondData. */
+  protected boolean hasDirectedEdge(T firstData, T secondData) {
+    // if (this.containsNodeHead(firstData)) {
+    //   int firstDataIndex = this.adjList.indexOf(new Node<T>(firstData));
+    //   return (this.adjList.get(firstDataIndex).nodeWithDataExists(secondData));
+    // }
     return false;
   }
 
-  /* Return true if both firstNode and secondNode point to each other. */
-  protected boolean hasUndirectedEdge(T firstNode, T secondNode) {
+  /* Return true if both firstData and secondData point to each other. */
+  protected boolean hasUndirectedEdge(T firstData, T secondData) {
     // return (
-    //   hasDirectedEdge(firstNode, secondNode) &&
-    //   hasDirectedEdge(secondNode, firstNode)
+    //   hasDirectedEdge(firstData, secondData) &&
+    //   hasDirectedEdge(secondData, firstData)
     // );
     return false;
   }
 
-  /* Add an edge from firstNode to secondNode. */
-  protected void addDirectedEdge(T firstNode, T secondNode) {
+  /* Add an edge from firstData to secondData. */
+  protected void addDirectedEdge(T firstData, T secondData) {
     /* Check if this node exists. */
-    if (this.containsNodeHead(firstNode)) {
-      this.adjacencyList.add(new Node<T>(firstNode));
-      System.out.println("found " + firstNode);
-    }
-    this.appendNodeAtIndex(new Node<T>(firstNode), new Node<T>(secondNode));
-    ++this.numEdges;
+    // if (this.containsNodeHead(firstData)) {
+    //   this.adjList.add(new Node<T>(firstData));
+    //   System.out.println("found " + firstData);
+    // }
+    // System.out.println("appending node ...");
+    // this.appendNodeAtIndex(new Node<T>(firstData), new Node<T>(secondData));
+    // ++this.numEdges;
     return;
   }
 
-  /* Add an edge from firstNode and secondNode, and
-     add another edge from secondNode to firstNode. */
-  protected void addUndirectedEdge(T firstNode, T secondNode) {
-    // this.addDirectedEdge(firstNode, secondNode);
-    // this.addDirectedEdge(secondNode, firstNode);
+  /* Add an edge from firstData and secondData, and
+     add another edge from secondData to firstData. */
+  protected void addUndirectedEdge(T firstData, T secondData) {
+    // this.addDirectedEdge(firstData, secondData);
+    // this.addDirectedEdge(secondData, firstData);
     // this.numEdges += 2;
     return;
   }
 
-  /* Remove the edge from firstNode to secondNode. */
-  protected void removeDirectedEdge(T firstNode, T secondNode) {
+  /* Remove the edge from firstData to secondData. */
+  protected void removeDirectedEdge(T firstData, T secondData) {
     return;
   }
 
-  /* Remove the edge from firstNode and secondNode, and
-     remove the edge from secondNode to firstNode. */
-  protected void removeUndirectedEdge(T firstNode, T secondNode) {
-    // this.removeDirectedEdge(firstNode, secondNode);
-    // this.removeDirectedEdge(secondNode, firstNode);
+  /* Remove the edge from firstData and secondData, and
+     remove the edge from secondData to firstData. */
+  protected void removeUndirectedEdge(T firstData, T secondData) {
+    // this.removeDirectedEdge(firstData, secondData);
+    // this.removeDirectedEdge(secondData, firstData);
     // this.numEdges -= 2;
     return;
   }
 
   /* Display the AdjacencyList in a readable format. */
   protected void show() {
-    for (Node n : this.adjacencyList) {
-      n.showTraverse();
+    HeadNode<T> curr = this;
+    while(curr != null) {
+      curr.next.showTraverse();
+      curr = curr.next;
     }
     return;
   }
@@ -77,30 +93,6 @@ class AdjacencyList<T> {
 
   /* Append the node at the linked list head determined by index. */
   private void appendNodeAtIndex(Node<T> head, Node<T> newNode) {
-    int headIndex = 0;
-    if (this.containsNodeHead(head.data)) {
-      headIndex = adjacencyList.indexOf(head);
-      this.adjacencyList.get(headIndex).appendToTail(newNode);
-    }
-
-    else {
-      this.adjacencyList.get(this.adjacencyList.size()).appendToTail(newNode);
-    }
     return;
-  }
-
-  /* Return true if there exists a node with value in the adjacency root mapping. */
-  private boolean containsNodeHead(T value) {
-    for (Node node : this.adjacencyList) {
-      if (node.data.equals(value)) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  /* Return the head node of the linked list at index. */
-  private Node getNodeAtIndex(int index) {
-    return this.adjacencyList.get(index);
   }
 }
---
+++ AdjacencyList/HeadNode.java
@@ -0,0 +1,15 @@
+
+public class HeadNode<T> extends Node {
+
+  protected T data;
+  protected Node<T> next;
+  protected HeadNode<T> down;
+
+  public HeadNode(T data) {
+    this.down = null;
+    this.next = null;
+    this.data = null;
+  }
+
+  protected 
+}
